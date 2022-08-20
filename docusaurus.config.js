afc864d8b654c9bfd31aa1715483486266614084
rhahao <rhahao.vj@gmail.com>
2022-08-20 18:26:28 +0300

    fix(docusaurus): update url and baseUrl to include trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
 	title: 'Scheduling Workbox System - Documentation',
-	url: 'https://your-docusaurus-test-site.com',
-	baseUrl: '/sws2apps-docs',
+	url: 'https://sws2apps.github.io',
+	baseUrl: '/sws2apps-docs/',
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
 	favicon: 'img/favicon.ico',
